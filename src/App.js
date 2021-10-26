3b92ca998719b77fc5bbe5cb67dca7fdb2ab6ad4
Student42069 <lehackdeshackers@gmail.com>
2021-10-26 01:08:02 -0400

    basic visuals and interactions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [lower, setLower] = useState("0");
+  const [upper, setUpper] = useState("0");
+
+  function handleChange({ target }) {
+    let but = target.innerHTML;
+    if (but === "AC") {
+      setLower("0");
+      setUpper("0");
+    } else if (but === "=") {
+    } else {
+      setLower(but);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="calculator">
+        <div id="display">
+          <div>
+            <p id="upper">{upper}</p>
+          </div>
+          <div>
+            <p id="lower">{lower}</p>
+          </div>
+        </div>
+        <div onClick={handleChange} id="pad">
+          <button id="clear">AC</button>
+          <button onClick={handleChange} id="divide">
+            /
+          </button>
+          <button id="multiply">*</button>
+          <button id="seven">7</button>
+          <button id="eight">8</button>
+          <button id="nine">9</button>
+          <button id="subtract">-</button>
+          <button id="four">4</button>
+          <button id="five">5</button>
+          <button id="six">6</button>
+          <button id="add">+</button>
+          <button id="one">1</button>
+          <button id="two">2</button>
+          <button id="three">3</button>
+          <button id="equals">=</button>
+          <button id="zero">0</button>
+          <button id="decimal">.</button>
+        </div>
+      </div>
     </div>
   );
 }
